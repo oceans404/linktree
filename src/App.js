ef5e3e8f8491b5357927a9d8c275a03395fe417b
Steph Orpilla <steph@Stephs-MacBook-Pro.local>
2022-06-20 11:41:21 -0400

    Add personal and professional links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const info = {
+    name: "Steph",
+    ethAddress: "oceans404.eth",
+    lensAddress: "stepho.lens",
+  };
+  const links = {
+    pfp: "https://ca.slack-edge.com/T8VPLGV4Z-U03HLD90VQC-9264ef1ef9df-512",
+    twitter: "https://twitter.com/0ceans404",
+    instagram: "https://www.instagram.com/_steph.js/",
+    lensprotocol: "https://www.lensfrens.xyz/stepho.lens",
+    telegram: "https://t.me/oceans404",
+    companyLink: "https://polygon.technology/",
+    polygonForm:
+      "https://docs.google.com/forms/d/e/1FAIpQLSfelhpuF-CLbq943b2FgkibJUmTauKoi3S-CNN7_pz-9xIGcA/viewform",
+    polygonDeveloperLibrary: "http://bit.ly/polygon-library",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div class="container">
+      <div class="circle-shape">
+        <a href={links.twitter} target="_blank" rel="noopener noreferrer">
+          <img src={links.pfp} alt={`${info.name}'s pfp`} class="profile-pic" />
+        </a>
+      </div>
+      <div className="info-div">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span className="light ">
+            {info.name} • {info.ethAddress} • {info.lensAddress}
+          </span>
         </p>
+        <p className="">
+          Developer Relations at
+          <a href={links.companyLink} target="_blank" rel="noopener noreferrer">
+            {` Polygon`}
+          </a>
+        </p>
+      </div>
+
+      <div class="link-list">
+        <a href={links.twitter} target="_blank" rel="noopener noreferrer">
+          <div class="link-list-item dark">Twitter</div>
+        </a>
+
+        <a href={links.telegram} target="_blank" rel="noopener noreferrer">
+          <div class="link-list-item dark">Telegram</div>
+        </a>
+
+        <a href={links.lensprotocol} target="_blank" rel="noopener noreferrer">
+          <div class="link-list-item dark">Lens Frens</div>
+        </a>
+
+        <a href={links.instagram} target="_blank" rel="noopener noreferrer">
+          <div class="link-list-item dark">Instagram</div>
+        </a>
+
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href={links.polygonDeveloperLibrary}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <div class="link-list-item dark">Polygon dev resources</div>
         </a>
-      </header>
+
+        <a href={links.polygonForm} target="_blank" rel="noopener noreferrer">
+          <div class="link-list-item dark">📄 Stay in touch with Polygon</div>
+        </a>
+      </div>
     </div>
   );
 }
